8d76f5ca9c00582753c3fecb4012d49b97c615de
Ricardo Canelas <hello@ricardocanelas.com>
2019-03-27 15:34:50 -0300

    Updates

---
+++ precache-manifest.a40547747e7544b58879fd6eddc2b2d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "860e465c2db058c1d0a5",
+    "url": "/challenge-laborit/static/js/runtime~main.9ee83c8c.js"
+  },
+  {
+    "revision": "5f7f2164290444d9d02c",
+    "url": "/challenge-laborit/static/js/main.518b75cd.chunk.js"
+  },
+  {
+    "revision": "08a88c21e35f2f8c42ff",
+    "url": "/challenge-laborit/static/js/2.3dc9270c.chunk.js"
+  },
+  {
+    "revision": "5f7f2164290444d9d02c",
+    "url": "/challenge-laborit/static/css/main.f17b5cc7.chunk.css"
+  },
+  {
+    "revision": "48577a18c8b28e23ddc7a92aa27f0917",
+    "url": "/challenge-laborit/index.html"
+  }
+];
